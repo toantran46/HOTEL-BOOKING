04b7b45b65965d81f02856dcb688ee466ec90bd5
vlinh2000 <vietlinhst2013@gmail.com>
2022-03-04 15:23:25 +0700

    done form search

--- client/src/features/Hotel/pages/MainPage/index.jsx
+++ client/src/features/Hotel/pages/MainPage/index.jsx
@@ -1,7 +1,9 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { Breadcrumb, BreadcrumbItem, Form, FormGroup, Input } from 'reactstrap';
 
-import "./MainPage.scss";
+import "./styles.scss";
+import { Link } from 'react-router-dom';
 
 MainPage.propTypes = {
 
@@ -9,8 +11,48 @@ MainPage.propTypes = {
 
 function MainPage(props) {
     return (
-        <div>
+        <div className='wrapper'>
+            <Breadcrumb className='wrapper__breadcrumb'>
+                <BreadcrumbItem className='wrapper__breadcrumb__item'>
+                    <Link to="/">Trang chủ</Link>
+                </BreadcrumbItem>
+                <BreadcrumbItem className='wrapper__breadcrumb__item'>
+                    <Link to="/">Việt Nam</Link>
+                </BreadcrumbItem>
+                <BreadcrumbItem className='wrapper__breadcrumb__item'>
+                    <Link to="/">Bà Rịa - Vũng Tàu</Link>
+                </BreadcrumbItem>
+                <BreadcrumbItem className='wrapper__breadcrumb__item'>
+                    <Link className='active' to="/">Kết quả tìm kiếm</Link>
+                </BreadcrumbItem>
+            </Breadcrumb>
+            <div className='wrapper__content'>
+                <div className='wrapper__content__left'>
+                    <div className='wrapper__content__left__form'>
+                        <p>Tìm</p>
+                        <Form>
+                            <FormGroup>
+                                <label>Tên chỗ nghỉ / điểm đến</label>
 
+                                <Input placeholder='' name="placeName" />
+                            </FormGroup>
+                            <FormGroup>
+                                <label>Ngày nhận phòng</label>
+                                <Input type="date" name='receiveDate' />
+                            </FormGroup>
+                            <FormGroup>
+                                <label>Ngày trả phòng</label>
+                                <Input type="date" name='returnDate' />
+                            </FormGroup>
+                            <button className='searchBtn'>Tìm</button>
+                        </Form>
+
+                    </div>
+
+
+                </div>
+                <div className='wrapper__content__right'></div>
+            </div>
         </div>
     );
 }
