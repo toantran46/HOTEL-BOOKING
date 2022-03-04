a361aa8f3e30b33efaf88d434247f8a749faf122
vlinh2000 <vietlinhst2013@gmail.com>
2022-03-04 18:49:02 +0700

    done UI list hotel page

--- client/src/features/Hotel/pages/MainPage/index.jsx
+++ client/src/features/Hotel/pages/MainPage/index.jsx
@@ -7,6 +7,8 @@ import { Link } from 'react-router-dom';
 
 import { MdSearch } from "react-icons/md";
 import FilterItem from 'features/Hotel/components/FilterItem';
+import HotelOverView from 'features/Hotel/components/HotelOverView';
+import Pagination from 'features/Hotel/components/Pagination';
 
 
 MainPage.propTypes = {
@@ -49,17 +51,73 @@ function MainPage(props) {
                             </FormGroup>
                             <button className='searchBtn'>Tìm</button>
                         </Form>
-
-                        <div className='wrapper__content__left__filter'>
-                            <p>Chọn lọc theo</p>
-                            <FilterItem />
-                        </div>
-
+                    </div>
+                    <div className='wrapper__content__left__filter'>
+                        <p className='title'>Chọn lọc theo</p>
+                        <FilterItem title="Sức khỏe & an toàn" items={[{ content: "Các chỗ nghỉ có biện pháp đảm bảo sức khỏe và an toàn", num: 124 }]} />
+                        <FilterItem
+                            title="Các bộ lọc phổ biến"
+                            items={
+                                [
+                                    { content: "Giáp biển", num: 302 },
+                                    { content: "Khách sạn", num: 203 },
+                                    { content: "Biệt thự", num: 7 },
+                                ]
+                            } />
+                        <FilterItem
+                            title="Xếp hạng sao"
+                            items={
+                                [
+                                    { content: "1 sao", num: 25 },
+                                    { content: "2 sao", num: 55 },
+                                    { content: "3 sao", num: 115 },
+                                    { content: "4 sao", num: 100 },
+                                    { content: "5 sao", num: 10 },
+                                ]
+                            } />
+                        <FilterItem
+                            title="Các hoạt động thú vị"
+                            items={
+                                [
+                                    { content: "Bãi biển", num: 342 },
+                                    { content: "Đi bộ đường dài", num: 114 },
+                                    { content: "Khu vực bãi tắm riêng", num: 144 },
+                                    { content: "Ca-nô", num: 104 },
+                                    { content: "Phòng tắm chung", num: 90 },
+                                    { content: "Massage", num: 55 },
+                                ]} />
+                        <FilterItem
+                            title="Điểm đánh giá của khách"
+                            items={
+                                [
+                                    { content: "Tuyệt hảo: 9 điểm trở lên", num: 342 },
+                                    { content: "Rất tốt: 8 điểm trở lên", num: 114 },
+                                    { content: "Tốt: 7 điểm trở lên", num: 144 },
+                                    { content: "Dễ chịu: 6 điểm trở lên", num: 104 },
+                                ]} />
+                        <FilterItem
+                            title="Tiện nghi phòng"
+                            items={
+                                [
+                                    { content: "Khu vực bếp", num: 342 },
+                                    { content: "Phòng tắm riêng", num: 114 },
+                                    { content: "Điều hòa không khí", num: 144 },
+                                    { content: "Bàn làm việc", num: 104 },
+                                    { content: "Bồn tắm", num: 90 },
+                                ]} />
                     </div>
 
 
                 </div>
-                <div className='wrapper__content__right'></div>
+                <div className='wrapper__content__right'>
+                    <p className='wrapper__content__right__search-result'>Vũng Tàu: tìm thấy 606 chỗ nghỉ</p>
+                    <HotelOverView />
+                    <HotelOverView />
+                    <HotelOverView />
+                    <HotelOverView />
+
+                    <Pagination />
+                </div>
             </div>
         </div>
     );
