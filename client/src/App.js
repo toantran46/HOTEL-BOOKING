782d3b059e6d3ca618c01af9238e875b0187ef4f
vlinh2000 <vietlinhst2013@gmail.com>
2022-03-04 13:48:21 +0700

    add routing in app.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
 import './App.css';
+
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import React, { Suspense } from 'react';
+
+const HoTel = React.lazy(() => import("./features/Hotel/index.jsx"));
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Suspense fallback={<div>loading...</div>}>
+        <BrowserRouter>
+          <Routes>
+            <Route index path='/*' element={<HoTel />} />
+            <Route path='/auth' element={<div>Auth</div>} />
+          </Routes>
+        </BrowserRouter>
+      </Suspense>
     </div>
   );
 }
---
+++ client/src/features/Hotel/index.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+Hotel.propTypes = {
+
+};
+
+function Hotel(props) {
+    return (
+        <div>
+            Hotel route
+        </div>
+    );
+}
+
+export default Hotel;
