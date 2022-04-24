a0e88546c69ff0085ad6a631b91c7b37aaabbff0
Hieu Nguyen <hieub1809236@student.ctu.edu.vn>
2022-04-24 16:46:15 +0700

    Add logic mobile icon

--- client/src/features/Admin/components/SideBar/index.jsx
+++ client/src/features/Admin/components/SideBar/index.jsx
@@ -4,9 +4,9 @@ import "./sidebar.scss";
 
 Sidebar.propTypes = {};
 
-function Sidebar(props) {
+function Sidebar({ widthSideBar }) {
   return (
-    <div className="sidebar">
+    <div className="sidebar" style={{ width: `${widthSideBar}px` }}>
       <div className="sidebar__header shadow-lg">
         <img
           className="sidebar__logo-name"
